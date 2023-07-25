140e5a206d0bd88e734d4204effcaaa42308ef18
cereceda1991 <cereceda1991@gmail.com>
2023-07-25 11:08:45 -0500

    feat : add email customize template

--- resources/views/emails/certificate.blade.php
+++ resources/views/emails/certificate.blade.php
@@ -1,10 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Certificate Ready</title>
+    <title>CertifyMe - ¡Felicidades!</title>
 </head>
 <body>
-    <p>Here is your certificate!. If you have any questions, please contact us through our contact channel! Greetings!</p>
-     <a href="{{$url}}">Click Here</a> 
+    <table width="100%" cellpadding="0" cellspacing="0">
+        <tr class="dynamic-bg">
+            <td colspan="2" width="50%" align="center" style="background-color: #f9f7f7; text-align: center; padding: 30px;">
+            <div class="certificate-container">
+    <p style="font-size: 26px; color: #000000; font-family: Montserrat, Arial, sans-serif;"><b>¡Congratulations! 🎉🎓</b></p>
+    <p style="font-size: 18px; color: #000000; font-family: Montserrat, Arial, sans-serif;">You have successfully completed our program. We want to celebrate your dedication and effort in this exciting learning journey. Your passion for acquiring new knowledge has led you to achieve this outstanding accomplishment. 💪</p>
+    <p style="font-size: 18px; color: #000000; font-family: Montserrat, Arial, sans-serif;">You are a true inspiration to all of us. 🚀</p>
+    <p style="font-size: 18px; color: #000000; font-family: Montserrat, Arial, sans-serif;">Thank you for choosing us as your platform to grow and develop. We are confident that your future successes will be even more extraordinary. 🌠</p>
+    <p style="font-size: 18px; color: #000000; font-family: Montserrat, Arial, sans-serif;">Keep shining and never stop learning! Knowledge is the key that opens all doors to your dreams. 🌈</p>
+    <br>
+    <a href="{{$url}}" style="background-color: #86E3CE; color: white; padding: 12px 28px; border-radius: 5px; text-decoration: none; font-size: 20px; font-family: Montserrat, Arial, sans-serif;">View Certificate</a>
+    <br><br>
+</div>
+
+            </td>
+        </tr>
+        <tr bgcolor="#FAC87B">
+            <td colspan="2" width="100%" height="80" align="center" style="background-color: #FAC87B;">
+                <div class="certificate-container">
+                    <p style="color: white; text-align: center; margin: 0; font-family: Montserrat, Arial, sans-serif;">2023 <b>CertifyMe</b>. Todos los derechos reservados.</p>
+                </div>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
